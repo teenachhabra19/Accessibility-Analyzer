4ac01251c4c1fb918b95b1a78b4012390e7f456b
Teena Chhabra <teenachhabra19@gmail.com>
2025-04-30 17:24:39 +0530

    Initial commit

---
+++ src/main/java/com/example/accessibility_analyzer/AccessibilityAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.accessibility_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessibilityAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessibilityAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/accessibility_analyzer/AccessibilityAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessibility_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessibilityAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
